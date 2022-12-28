4cb136e550330c6c509911f8ecb4985d15ab6106
Kerem Ali GÜNER <61164515+linkincorc@users.noreply.github.com>
2022-12-28 15:23:36 +0300

    AfterLogin

---
+++ WebProgramlamaProje2/Areas/Identity/Data/WebProjeDBContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebProgramlamaProje.Areas.Identity.Data;
+
+namespace WebProgramlamaProje.Areas.Identity.Data;
+
+public class WebProjeDBContext : IdentityDbContext<WebProgramlamaProjeUser>
+{
+    public WebProjeDBContext(DbContextOptions<WebProjeDBContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<WebProgramlamaProjeUser>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<WebProgramlamaProjeUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+    }
+}
+
+
+
